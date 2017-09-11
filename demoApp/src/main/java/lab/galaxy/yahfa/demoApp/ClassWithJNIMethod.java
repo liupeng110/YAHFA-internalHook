4408ff39ad3fa21ff2dcf9a4648c91ab43819dbf
鹏 <717219917@qq.com>
2017-09-11 17:55:01 +0800

    Update ClassWithJNIMethod.java

--- demoApp/src/main/java/lab/galaxy/yahfa/demoApp/ClassWithJNIMethod.java
+++ demoApp/src/main/java/lab/galaxy/yahfa/demoApp/ClassWithJNIMethod.java
@@ -1,1 +1,14 @@
 
+package lab.galaxy.yahfa.demoApp;
+
+/**
+ * Created by lrk on 5/4/17.
+ */
+
+public class ClassWithJNIMethod {
+    static {
+        System.loadLibrary("hello");
+    }
+
+    public native static String fromJNI();
+}
